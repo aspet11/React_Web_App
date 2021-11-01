5beb8a456ae289fbb6ba6c12c9900b3423f2a7ba
aspet11 <aspet11manukyan@gmail.com>
2021-11-01 20:24:14 +0300

    Updated root path.

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter, Route} from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-    <BrowserRouter basename={"/tf-jsx-demo/exolot/"}>
+    <BrowserRouter basename={"/"}>
         <Route path="/" component={App} />
     </BrowserRouter>, 
 document.getElementById('root'));
